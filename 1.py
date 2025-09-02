2018489b97b25632b6c3fc2edb9e7b40129fe458
articuona-max <aravind22105@gmail.com>
2025-09-03 00:00:13 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,15 @@
+import textblob as t
+print("------Welcome all to This Sentiment Analysis Page------")
+user_i=input("Enter the desired passage :\n")
+analysis = TextBlob(user_i)
+print(analysis.sentiment)
+polarity = analysis.sentiment.polarity
+if(polarity>0):
+    sentiment="Positive"
+elif(polarity<0):
+    sentiment="negative"
+else:
+    sentiment="Neutral"
+print(f"The final results are here!")
+print(f"The polarity of passage is : {polarity}")
+print(f"The Sentiment is :{sentiment}")
